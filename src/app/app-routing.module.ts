c822389149f6ce92e7144fd9ac38425e421b7247
kevinsamuel1510 <kevinrockz250@gmail.com>
2023-11-17 10:56:53 +0530

    landingpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'login',component:loginComponent {
+  {path:'login',component:login Component {
     
   },
 ];
